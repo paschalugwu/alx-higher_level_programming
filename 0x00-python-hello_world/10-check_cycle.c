e30918d851799a1736df7e003241cff30c90a343
root <root@DESKTOP-CDE41BB.localdomain>
2023-10-30 16:03:20 +0100

    feat(cycle): Add C function to check for linked list cycle
    Implement the  check_cycle  function in C, which checks if a singly linked list has a cycle in it.
    
    - Add the  check_cycle  function to the  10-check_cycle.c  file, as specified in the task.
    - Update the  lists.h  header file with the function prototype.

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,26 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
- * @list: pointer to the head of the linked list
- * Return: 0 if there is no cycle, 1 if there is a cycle
-*/
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	if (list == NULL || list->next == NULL)
+	if (!list)
 		return (0);
 
-	slow = list; /* Set slow pointer to the head of the list */
-	fast = list->next; /* Set fast pointer to the next node */
-
-	while (fast != NULL && fast->next != NULL)
+	while (slow && fast && fast->next)
 	{
-		if (slow == fast) /*If slow and fast pointers meet, there is a cycle */
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
-
-		slow = slow->next; /* Move slow pointer one step forward */
-		fast = fast->next->next; /* Move fast pointer two steps forward */
 	}
-	return (0); /* If loop completes, there is no cycle */
+
+	return (0);
 }
