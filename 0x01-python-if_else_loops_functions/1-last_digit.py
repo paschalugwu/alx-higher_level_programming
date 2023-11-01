b2676991deaabc74d1c663e34d8b4f15262429ef
root <root@DESKTOP-CDE41BB.localdomain>
2023-11-01 01:41:27 +0100

    feat(commit_message): Add efficient code solution for finding the last digit of a number
    - Implemented the required code to determine the last digit of a randomly generated number.
    - Used the modulus operator to calculate the last digit.
    - Printed the last digit along with additional information based on its value.
    - Tested the code with various inputs to ensure it provides accurate results.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
-print("Last digit of", number, "is", last_digit, end=" ")
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+print(f"The last digit of {number:d} is {digit:d} and the number is ", end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("zero")
 else:
-    print("and is less than 6 and not 0")
+    print("less than 6 and not equal to 0")
