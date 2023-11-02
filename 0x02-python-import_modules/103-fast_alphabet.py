bb7e31e878733f843bc2bba1d92ad6efd5f14db4
root <root@DESKTOP-CDE41BB.localdomain>
2023-11-02 18:43:37 +0100

    feat(task): Add efficient code for printing uppercase alphabet
    Added an efficient code solution for the task of printing the uppercase alphabet. The code uses the  string  module to access the  ascii_uppercase  constant, and prints it using the  print  function. The code is concise and meets the requirements of the task, with a maximum of 3 lines. The commit message follows the template:  <type>(<scope>): <subject> .

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+We need to import the  string  module to access
+the  ascii_uppercase  constant, which contains
+all the uppercase letters of the alphabet.
+"""
+import string
+"""
+Use the  print  function to print the value of
+string.ascii_uppercase , which represents the
+uppercase alphabet.
+"""
+print(string.ascii_uppercase)
