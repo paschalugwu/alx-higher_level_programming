7f9230ada1bd98ba8b578ea393d6f7dd69b5ed72
root <root@DESKTOP-CDE41BB.localdomain>
2023-11-02 13:15:23 +0100

    feat(commit): Add script to access and print command-line arguments
    Added a script to access and print command-line arguments. The script uses the  sys  module to access the arguments and includes a condition to ensure it is only executed when the script is run directly. It also checks the number of arguments and prints them in the specified format.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -23,5 +23,3 @@ if __name__ == "__main__":
     if num_args > 0:
         for i, arg in enumerate(sys.argv[1:], start=1):
             print("{}: {}".format(i, arg))
-    else:
-        print("No arguments.")
