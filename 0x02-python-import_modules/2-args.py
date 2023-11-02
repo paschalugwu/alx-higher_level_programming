57251e2d6a296330ce72179de65a0a3cb9c1217d
root <root@DESKTOP-CDE41BB.localdomain>
2023-11-02 13:28:02 +0100

    feat(commit): Add script to access and print command-line arguments
    Added a script to access and print command-line arguments. The script uses the  sys  module to access the arguments and includes a condition to ensure it is only executed when the script is run directly. It also checks the number of arguments and prints them in the specified format.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-
-
-"""
-Use the  if __name__ == "__main__":  condition to ensure
-the code is only executed when the script is run directly,
-not when imported as a module
-"""
 if __name__ == "__main__":
-
-    # Import the  sys  module to access the command-line arguments
+    """Print the number of and list of arguments."""
     import sys
 
-    # Get the number of arguments passed to the script using  len(sys.argv)
-    num_args = len(sys.argv) - 1
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
 
-    # Print the number of arguments in the specified format
-    print("{} argument{}:".format(num_args, "s" if num_args != 1 else ""))
-
-    """
-    Check if there are any arguments and iterate over them using a
-    for  loop
-    """
-    if num_args > 0:
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print("{}: {}".format(i, arg))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
