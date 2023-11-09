438bd4108ad7f2418a071ec4092bec27d1d2eb9a
root <root@DESKTOP-CDE41BB.localdomain>
2023-11-09 03:53:33 +0100

    feat(multiply_list_map): Implement function to multiply elements in a list using map
    - Define the function  multiply_list_map  that takes in two parameters:  my_list  (with a default value of an empty list) and  number  (with a default value of 0).
    - Inside the function, use the  map  function to apply a lambda function to each element of  my_list . The lambda function multiplies each element by the given  number .
    - Return a new list containing the results of the map operation.
    
    This commit implements the  multiply_list_map  function to efficiently multiply each element in a list by a given number using the  map  function, following the given requirements.

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-"""
-Let's start by defining the function multiply_list_map that
-takes in two parameters: my_list (with a default value of an
-empty list) and number (with a default value of 0)
-"""
-
-
 def multiply_list_map(my_list=[], number=0):
-    """
-    Inside the function, we use the map function to apply a lambda
-    function to each element of my_list. The lambda function
-    multiplies each element by the given number
-    """
     return (list(map((lambda i: i * number), my_list)))
