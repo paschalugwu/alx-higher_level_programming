9a5fb08b7374daa1de5eca8224b3cbc07aef515d
root <root@DESKTOP-CDE41BB.localdomain>
2023-11-21 00:36:14 +0100

    feat(function): Add raise_exception function
    Add raise_exception function to raise a TypeError exception. The function does not import any modules and efficiently raises the exception using the  raise  keyword.

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The task requires you to write a function called raise_exception
+that raises a TypeError exception. You are not allowed to import
+any module. Let's start by defining the function raise_exception"""
+
+
+def raise_exception():
+    """Inside the function, use the raise keyword
+    to raise a TypeError exception"""
+    raise TypeError
