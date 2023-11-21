9d9b7e0123d4ddcdd15202656ba4236b491226e6
paschalugwu <ugwupaschal@gmail.com>
2023-11-21 23:42:56 +0100

    feat(class): Add Square class
    Add Square class to define an empty class that represents a square. The class is defined without importing any modules. It includes a docstring to provide a description of the class. The pass statement is used to indicate that there are no other class members.

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The task requires you to write an empty class called square
+that defines a square. You are not allowed to import any modules.
+You start by defining the class square"""
+
+
+class Square:
+    """Add the pass statement inside the clss to indicate that there
+    is no other class members"""
+    pass
