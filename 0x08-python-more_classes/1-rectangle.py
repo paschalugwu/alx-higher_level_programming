605d9f386ac408b633a1b550693a92cee9ff3eb3
paschalugwu <ugwupaschal@gmail.com>
2023-11-27 15:50:34 +0100

    feat(class): Add Rectangle class
    Add Rectangle class to define a rectangle with private instance attributes width and height. The class efficiently includes getter and setter methods for both attributes to retrieve and set their values. The getter methods retrieve the values of the attributes, while the setter methods set the values after performing necessary checks. The class can be instantiated with optional width and height parameters. No modules are imported in the implementation.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,12 +14,8 @@ class Rectangle:
     height as optional parameters"""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
+        """Inside the __init__ method, initialize the private instance
+        attributes __width and __height with the values of width and height"""
         self.__width = width
         self.__height = height
         """Define the getter method to retrieve the value of the __width"""
@@ -27,10 +23,10 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-        """Define the setter method width to set the value of the __width
-        attribute. Perform necessary checks to ensure that the value is an
-        integer and not less than 0. Raise TypeError and ValueError exceptions
-        if the checks fail"""
+    """Define the setter method width to set the value of the __width
+    attribute. Perform necessary checks to ensure that the value is an
+    integer and not less than 0. Raise TypeError and ValueError exceptions
+    if the checks fail"""
 
     @width.setter
     def width(self, value):
