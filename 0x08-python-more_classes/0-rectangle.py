e86e0b9c4391149e27c99aff841dd7ddac8f85ac
paschalugwu <ugwupaschal@gmail.com>
2023-11-27 14:58:34 +0100

    feat(class): Add Rectangle class
    Add Rectangle class to define a rectangle. The class is empty and does not import any modules. It represents a rectangle and includes a docstring to provide a brief description. The pass statement indicates that there is no further code in the class.

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This task requires you to write an empty class called Rectangle
+that defines a rectangle. You should not import any modules.
+Start by defining te class"""
+
+
+class Rectangle:
+    """Next, add the pass statement: indicates no further code in the class"""
+    pass
