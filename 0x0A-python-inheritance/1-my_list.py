a7f594f55d8755f43f54081411953eeaa19b62f8
paschalugwu <ugwupaschal@gmail.com>
2023-12-04 23:50:24 +0100

    feat(class): Add MyList class
    Add MyList class that represents a custom list inheriting from the built-in list class. The class includes a print_sorted method that prints the elements of the list in ascending order. The method uses the sorted function to sort the elements before printing them.

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+"""
+Defines MyList that inherits from list
+"""
 
 
 class MyList(list):
     """
-    This class represents a custom list that
-    inherits from the built-in list class.
+        This class inherits from list.
+        Attributes:
+        Methods:
+            print_sorted - prints the list in ascending order
     """
     def print_sorted(self):
         """
-        Prints the elements of the list in ascending order.
+           prints a list in ascending order.
         """
         print(sorted(self))
-        """
-        Sorts the elements of the list in ascending order and prints them
-        """
