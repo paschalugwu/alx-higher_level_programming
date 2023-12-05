c659a8b8b4fc0d7dac3e907001c7217678c3fa64
paschalugwu <ugwupaschal@gmail.com>
2023-12-05 10:03:57 +0100

    feat(class): Add Rectangle class
    Add Rectangle class that inherits from the BaseGeometry class. The Rectangle class represents a rectangle and has attributes for width and height. It initializes the instance with width and height arguments, performs integer validation using the integer_validator method from the BaseGeometry class, and assigns the validated values to the private attributes __width and __height.

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Write a Rectangle class that inherits from BaseGeometry class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Represents a rectangle.
+    Inherits from BaseGeometry.
+    """
+    def __init__(self, width, height):
+        """
+        Initializes an instance.
+
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
