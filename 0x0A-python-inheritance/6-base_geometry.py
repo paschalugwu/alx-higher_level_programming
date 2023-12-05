71669087aaf00f92f624674d694e58b03522537b
paschalugwu <ugwupaschal@gmail.com>
2023-12-05 02:48:33 +0100

    feat(class): Add BaseGeometry class improvement
    Add BaseGeometry class improvement to represent base geometry. The class includes an area() method that is not implemented and raises an exception with the message 'area() is not implemented'. The code follows the standard docstring comments for clear explanation of the class and method functionalities.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Improvement of the BaseGeometry"""
+
+
+class BaseGeometry:
+    """Represent nase geometry"""
+    def area(self):
+        """Not implemented"""
+        raise Exception('area() is not implemented')
