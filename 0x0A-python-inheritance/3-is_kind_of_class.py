7762051b53609525f0ad3d79fbeaa5d444ab4da8
paschalugwu <ugwupaschal@gmail.com>
2023-12-05 02:04:10 +0100

    feat(function): Add is_kind_of_class function
    Add is_kind_of_class function to check if an object is an instance or inherited instance of a class. The function efficiently uses the isinstance() function to perform the check and returns True if the object matches the specified class or an inherited class. Otherwise, it returns False. The function includes standard docstring comments to explain its purpose, arguments, and return value.

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,16 +1,17 @@
-#!/usr/bin/pythyon3
-"""Check instance of class"""
+#!/usr/bin/python3
+"""
+3-is_kind_of_class module
+"""
 
 
 def is_kind_of_class(obj, a_class):
-    """Checks if an object is an instance or inherited instance of a class.
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is an instance or inherited instance of a_class - True.
-        Otherwise - False
     """
-    if isinstance(obj, a_class):
-        return True
-    return False
+    is_kind_of_class: Check if an object is an instance,
+                    or inherited instance of a class.
+    Args:
+        obj: The object to check.
+        a_class: The class to match the type of obj to.
+    Returns: If obj is an instance or inherited instance of a_class - True,
+            Otherwise - False.
+    """
+    return isinstance(obj, a_class)
