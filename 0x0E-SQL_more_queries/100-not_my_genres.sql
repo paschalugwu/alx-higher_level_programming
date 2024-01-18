d42f1d59e7bb5d41fb6b753e82a99ae09710885d
paschalugwu <ugwupaschal@gmail.com>
2024-01-18 15:43:37 +0100

    feat(sql): implement script to list genres not linked to Dexter
    Implemented SQL script (100-not_my_genres.sql) to efficiently retrieve and list all genres not linked to the show Dexter in the hbtn_0d_tvshows database. The script uses two SELECT statements and a NOT IN clause to filter out genres linked to Dexter, ensuring the results are sorted in ascending order by genre name.

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,25 @@
+-- Lists all genres not linked to the show Dexter
+-- Retrieve distinct genres linked to the show Dexter
+SELECT DISTINCT `name`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
+
+       INNER JOIN `tv_shows` AS t
+       ON s.`show_id` = t.`id`
+       WHERE t.`title` = "Dexter";
+
+-- Retrieve all distinct genres
+SELECT DISTINCT `name`
+  FROM `tv_genres`
+  WHERE `name` NOT IN
+        -- Exclude genres linked to the show Dexter
+        (SELECT DISTINCT `name`
+           FROM `tv_genres` AS g
+                INNER JOIN `tv_show_genres` AS s
+                ON g.`id` = s.`genre_id`
+    
+                INNER JOIN `tv_shows` AS t
+                ON s.`show_id` = t.`id`
+                WHERE t.`title` = "Dexter")
+ORDER BY `name`;
