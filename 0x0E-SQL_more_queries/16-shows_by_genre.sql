642591a53277c4571a599677abc32d004ea1d6a9
paschalugwu <ugwupaschal@gmail.com>
2024-01-18 15:18:49 +0100

    feat(sql): implement script to list shows and genres
    Implemented SQL script (16-shows_by_genre.sql) to efficiently retrieve and list all shows and their linked genres from the hbtn_0d_tvshows database. The script uses LEFT JOIN operations to connect the tv_shows, tv_show_genres, and tv_genres tables, displaying NULL for shows without genres and sorting the results by show title and genre name.

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- Lists all shows and genres
+SELECT t.`title`, g.`name`
+  FROM `tv_shows` AS t
+       -- Left join with tv_show_genres table
+       LEFT JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+ 
+       -- Left join with tv_genres table
+       LEFT JOIN `tv_genres` AS g
+       ON s.`genre_id` = g.`id`
+ORDER BY t.`title`, g.`name`;
