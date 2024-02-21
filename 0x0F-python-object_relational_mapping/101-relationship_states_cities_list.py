c88d6b2be03ccd150f8d4008d195f576db45ca15
paschalugwu <ugwupaschal@gmail.com>
2024-02-21 06:02:52 +0100

    feat(script): Add script to list State-City relationships
    Write a Python script that lists all State objects along with their corresponding City objects in the provided MySQL database using SQLAlchemy. The script takes three arguments: MySQL username, MySQL password, and database name. It connects to localhost on port 3306 and retrieves data with a single query, utilizing the cities relationship for all State objects. Results are sorted in ascending order by states.id and cities.id as specified. Script does not execute when imported.

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-'''script for task 16'''
+"""Lists all States and corresponding Cities in the database hbtn_0e_101_usa.
+Usage: ./101-relationship_states_cities_list.py <mysql username> /
+                                                <mysql password> /
+                                                <database name>
+"""
 
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State, Base
+from relationship_state import State
 from relationship_city import City
-import sys
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    host = 'localhost'
-    port = '3306'
-
-    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
-                            username, password, host, port, db_name
-                            ), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    loc_session = Session()
-    states = loc_session.query(State).order_by(State.id.asc()).all()
+    session = Session()
 
-    for state in states:
-        print('{}: {}'.format(state.id, state.name))
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print('\t{}: {}'.format(city.id, city.name))
-
-    loc_session.close()
-    engine.dispose()
+            print("    {}: {}".format(city.id, city.name))
