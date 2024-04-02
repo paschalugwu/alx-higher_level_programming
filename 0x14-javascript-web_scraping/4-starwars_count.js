7a215672234e903e79e9b4bacc8226994a05e316
paschalugwu <ugwupaschal@gmail.com>
2024-04-02 17:47:30 +0100

    feat(starwars-count): Add script to count movies featuring Wedge Antilles
    Implemented a Node.js script that sends a GET request to the specified Star Wars API URL and filters the results to count the number of movies where the character 'Wedge Antilles' (character ID 18) is present. The count is then printed to the console.

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+// Importing the 'request' module to make HTTP requests
+const request = require('request');
+
+// Sending a GET request to the specified Star Wars API URL
+request(process.argv[2], function (error, response, body) {
+  // Checking if there are no errors during the request process
+  if (!error) {
+    // Parsing the response body as JSON and accessing the 'results' property
+    const results = JSON.parse(body).results;
+    
+    // Filtering the movies to count the number of movies where "Wedge Antilles" is present
+    const count = results.reduce((count, movie) => {
+      // Checking if "Wedge Antilles" character ID (18) is present in the characters array of the movie
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1  // Incrementing the count if "Wedge Antilles" is found in the characters array
+        : count;     // Keeping the count unchanged if "Wedge Antilles" is not found
+    }, 0);
+
+    // Printing the total count of movies where "Wedge Antilles" is present
+    console.log(count);
+  }
+});
